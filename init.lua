667016e6ce800dd8e71a0caefd10b42422f7e8b4
Vinko Kodzoman <vinko.kodzoman@yahoo.com>
2025-05-15 21:24:24 -0700

    Initial setup lazy + telescope

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim with plugins
+require("lazy").setup({
+  spec = {
+    -- Your other plugins here if any
+
+    -- Add Telescope plugin
+    {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.5", -- pinned version
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require("telescope").setup()
+      end,
+    },
+  },
+  -- global lazy.nvim settings
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+})
+
+-- Keymaps for Telescope (can be after setup)
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
